7a45c2247322c5cc1c57b71ba8c3c778256ff112
Kristoffer Dalby <kradalby@kradalby.no>
2015-07-29 07:09:34 +0200

    derp

--- roles/config_server/templates/update.sh
+++ roles/config_server/templates/update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 hostname=$(hostname)
+domain=$(hostname -d)
 ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 os=$(lsb_release -d -s)
-uptime=$(uptime)
+uptime=$(uptime -p)
+load=$(cat /proc/loadavg)
 
 curl -i --silent \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--X POST --data '{"internal":"'"$ip"'","hostname":"'"$hostname"'","os":"'"$os"'","uptime":"'"$uptime"'"}' \
+-X POST --data '{"internal":"'"$ip"'","hostname":"'"$hostname"'","domain":"'"$domain"'","os":"'"$os"'","uptime":"'"$uptime"'","load":"'"$load"'"}' \
 "https://fap.no/api/v1/server/update"
