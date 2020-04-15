6e6e05d4183307e3899f3927ba56c94f9b4b456b
Kristoffer Dalby <kradalby@kradalby.no>
2020-04-15 16:55:54 +0100

    Register the multicast socat sender

--- roles/mdns-socat-tunnel/templates/socat_mdns_sender.sh
+++ roles/mdns-socat-tunnel/templates/socat_mdns_sender.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-socat -u UDP4-RECVFROM:5353,reuseaddr,fork - | tee \
+socat -u UDP4-RECVFROM:5353,ip-add-membership=224.0.0.251:{{ ansible_default_ipv4.address|default(ansible_all_ipv4_addresses[0]) }},reuseaddr,fork - | tee \
     {% for host in groups['socat_mdns'] | difference(inventory_hostname) %}
     >(socat -u - UDP4-SENDTO:{{ host }}:55353) \
     {% endfor %}
