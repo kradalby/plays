998dc318b5f1daeff62464ada950686cb7cd8065
Kristoffer Dalby <kradalby@kradalby.no>
2020-10-14 21:15:24 +0100

    Try to improve socat mdns setup

--- roles/mdns-socat-tunnel/templates/socat_mdns_sender.sh
+++ roles/mdns-socat-tunnel/templates/socat_mdns_sender.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # {{ ansible_managed }}
 
-socat -u UDP4-RECVFROM:5353,ip-add-membership=224.0.0.251:{{ ansible_default_ipv4.address|default(ansible_all_ipv4_addresses[0]) }},reuseaddr,fork - | tee \
+socat -u UDP4-RECVFROM:5353,ip-add-membership=224.0.0.251:{{ mdns_override_ip }},reuseaddr,fork - | tee \
     {% for host in groups['socat_mdns'] | difference(inventory_hostname) %}
     >(socat -u - UDP4-SENDTO:{{ host }}:55353) \
     {% endfor %}
