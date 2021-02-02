d16abbc1a271990754244af58b598147471caae7
Kristoffer Dalby <kradalby@kradalby.no>
2021-02-02 17:51:49 +0000

    virt, nfs, restic

--- roles/restic_backup_job/templates/backup_restic.sh
+++ roles/restic_backup_job/templates/backup_restic.sh
@@ -2,6 +2,10 @@
 
 {% set excludes = restic_backup_job_excludes | map('regex_replace', '^', '--exclude=') | list | join(" ") -%}
 
+ACTION=${1:-backup}
+
+SECONDS=0
+
 set -euo pipefail
 
 RESTIC=/usr/local/bin/restic
@@ -11,23 +15,31 @@ export RESTIC_REPOSITORY='{{ item }}'
 
 echo "Doing backup to $RESTIC_REPOSITORY"
 
-$RESTIC snapshots > /dev/null || $RESTIC init
+if [ "$ACTION" = "backup" ] || [ "$ACTION" = "full" ]; then
+    $RESTIC snapshots > /dev/null || $RESTIC init
 
-$RESTIC unlock
+    $RESTIC unlock
 
-$RESTIC backup --verbose {{ excludes }} {{ restic_backup_job_directories | join(" ") }}
+    $RESTIC backup --verbose {{ excludes }} {{ restic_backup_job_directories | join(" ") }}
 
-$RESTIC check
-$RESTIC snapshots
+    if [ "$ACTION" = "full" ]; then
+        $RESTIC check
+        $RESTIC snapshots
 
-$RESTIC forget \
-    --keep-hourly 8 \
-    --keep-daily 7 \
-    --keep-weekly 4 \
-    --keep-monthly 6 \
-    --keep-yearly 10
+        $RESTIC forget \
+            --keep-hourly 8 \
+            --keep-daily 7 \
+            --keep-weekly 4 \
+            --keep-monthly 6 \
+            --keep-yearly 10
 
-$RESTIC prune
+        $RESTIC prune
+    fi
+fi
+
+DURATION=$SECONDS
+
+echo "Total time spend is pasted below:"
+echo "$(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds elapsed."
 
 exit 0
-
