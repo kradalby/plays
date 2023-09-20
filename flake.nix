5cf4deb4898af69f30ea8927f4f2bf4c18db3dfd
Kristoffer Dalby <kristoffer@tailscale.com>
2023-09-20 09:31:43 -0500

    flake: add python and toml
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- flake.nix
+++ flake.nix
@@ -4,44 +4,50 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      infrastructureVersion = if (self ? shortRev) then self.shortRev else "dev";
-    in
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    infrastructureVersion =
+      if (self ? shortRev)
+      then self.shortRev
+      else "dev";
+  in
     {
-      overlay = final: prev:
-        let
-          pkgs = nixpkgs.legacyPackages.${prev.system};
-        in
-        rec { };
-    } // flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            overlays = [ self.overlay ];
-            inherit system;
-          };
-          buildDeps = with pkgs; [
-            ansible
-          ];
-          devDeps = with pkgs;
-            buildDeps ++
-            (with elmPackages;
-            [
-              ansible-lint
-            ]);
+      overlay = final: prev: let
+        pkgs = nixpkgs.legacyPackages.${prev.system};
+      in rec {};
+    }
+    // flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        overlays = [self.overlay];
+        inherit system;
+      };
+      buildDeps = with pkgs; [
+        ansible
 
-        in
-        rec {
-          # `nix develop`
-          devShell = pkgs.mkShell { buildInputs = devDeps; };
+        python310Full
+        python310Packages.toml
+      ];
+      devDeps = with pkgs;
+        buildDeps
+        ++ (with elmPackages; [
+          ansible-lint
+        ]);
+    in rec {
+      # `nix develop`
+      devShell = pkgs.mkShell {buildInputs = devDeps;};
 
-          checks = {
-            format = pkgs.runCommand "check-format"
-              {
-                buildInputs = devDeps;
-              } ''
-            '';
-          };
-        });
+      checks = {
+        format =
+          pkgs.runCommand "check-format"
+          {
+            buildInputs = devDeps;
+          } ''
+          '';
+      };
+    });
 }
